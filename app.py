cd650e8e4884d4cc57d526e1bc254ad72c122b42
CherryBlue25 <161093980+CherryBlue25@users.noreply.github.com>
2024-02-24 11:19:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
 %%writefile app.py
 import streamlit as st
 from openai import OpenAI
-import secret_keys
 
-
-client = OpenAI(api_key = secret_keys.openai_api_key)
-
+client = OpenAI(api_key = secrets_keys.OpenAI_api_key)
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-  {"role": "system", "content": "Piensa como un Desarrollador"}
+  {"role": "system", "content": "Piens como un inge"}
 ]
-
 
 def communicate():
     messages = st.session_state["messages"]
     user_message = {"role": "user", "content": st.session_state["user_input"]}
     messages.append(user_message)
 
-
-    response = client.chat.completions.create(
+        response = client.chat.completions.create(
       model="gpt-3.5-turbo",
       messages=messages
     )
